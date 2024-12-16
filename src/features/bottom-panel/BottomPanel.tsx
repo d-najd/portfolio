8fe6f69d22f3047dd076a4b911077b1712e94141
d-najd <dimitar.hristijan.najdovski@gmail.com>
2024-12-16 23:17:49 +0100

    work on bottom bar

--- src/features/bottom-panel/BottomPanel.tsx
+++ src/features/bottom-panel/BottomPanel.tsx
@@ -1,11 +1,16 @@
 import styles from "./BottomPanel.module.scss"
+import commonStyles from "../../components/common/commonStyles.module.scss"
 
 export const BottomPanel = () => {
 	return (
 		<>
 			<div className={styles.container}>
 				<div className={styles.bottomBar}>
-					TEST (C) 2010
+					<div className={commonStyles.verticalCenter}>
+						<button className={commonStyles.button}>
+							<img className={styles.widowsImage} src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Windows_Logo_%281992-2001%29.svg" alt={"Windows 95 logo"}/>
+						</button>
+					</div>
 				</div>
 			</div>
 		</>
