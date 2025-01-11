384205aae0446ff7e799225dd21ffc0d33beec0d
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-01-11 22:15:40 +0100

    feat!: refactoring window drawer so that I can properly test it

--- src/components/useScreenSize.ts
+++ src/components/useScreenSize.ts
@@ -1,29 +1,34 @@
 // useScreenSize.js
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from "react"
 
-const useScreenSize = () => {
+export interface ScreenSize {
+	width: number
+	height: number
+}
+
+const useScreenSize = (): ScreenSize => {
 	const [screenSize, setScreenSize] = useState({
 		width: window.innerWidth,
-		height: window.innerHeight,
-	});
+		height: window.innerHeight
+	})
 
 	useEffect(() => {
 		const handleResize = () => {
 			setScreenSize({
 				width: window.innerWidth,
-				height: window.innerHeight,
-			});
-		};
+				height: window.innerHeight
+			})
+		}
 
-		window.addEventListener('resize', handleResize);
+		window.addEventListener("resize", handleResize)
 
 		// Clean up the event listener when the component unmounts
 		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, []);
+			window.removeEventListener("resize", handleResize)
+		}
+	}, [])
 
-	return screenSize;
-};
+	return screenSize
+}
 
-export default useScreenSize;
+export default useScreenSize
