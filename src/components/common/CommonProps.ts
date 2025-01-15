30c56f373bfce9cb54db8adad54327e602f1546b
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-01-15 22:58:05 +0100

    feat: added utility for aligning-self

--- src/components/common/CommonProps.ts
+++ src/components/common/CommonProps.ts
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react"
-import type { SerializedStyles } from "@emotion/react";
+import type { SerializedStyles } from "@emotion/react"
 import { css } from "@emotion/react"
 
 /**
@@ -23,45 +23,53 @@ export enum Alignments {
 	CenteredStart = "centered-start",
 	CenteredEnd = "centered-end",
 	BottomStart = "bottom-start",
-	BottomEnd = "bottom-end",
+	BottomEnd = "bottom-end"
+}
+
+const justifyContentMap: Record<Alignments, string> = {
+	[Alignments.Top]: "initial",
+	[Alignments.Bottom]: "initial",
+	[Alignments.Start]: "flex-start",
+	[Alignments.End]: "flex-end",
+	[Alignments.VerticallyCentered]: "initial",
+	[Alignments.HorizontallyCentered]: "center",
+	[Alignments.Centered]: "center",
+	[Alignments.TopStart]: "flex-start",
+	[Alignments.TopEnd]: "flex-end",
+	[Alignments.CenteredStart]: "flex-start",
+	[Alignments.CenteredEnd]: "flex-end",
+	[Alignments.BottomStart]: "flex-start",
+	[Alignments.BottomEnd]: "flex-end"
+}
+
+const alignItemsMap: Record<Alignments, string> = {
+	[Alignments.Top]: "flex-start",
+	[Alignments.Bottom]: "flex-end",
+	[Alignments.Start]: "initial",
+	[Alignments.End]: "initial",
+	[Alignments.VerticallyCentered]: "center",
+	[Alignments.HorizontallyCentered]: "initial",
+	[Alignments.Centered]: "center",
+	[Alignments.TopStart]: "flex-start",
+	[Alignments.TopEnd]: "flex-start",
+	[Alignments.CenteredStart]: "center",
+	[Alignments.CenteredEnd]: "center",
+	[Alignments.BottomStart]: "flex-end",
+	[Alignments.BottomEnd]: "flex-end"
 }
 
 export const Alignment = (alignment: Alignments): SerializedStyles => {
-	const justifyContentMap: Record<Alignments, string> = {
-		[Alignments.Top]: "initial",
-		[Alignments.Bottom]: "initial",
-		[Alignments.Start]: "flex-start",
-		[Alignments.End]: "flex-end",
-		[Alignments.VerticallyCentered]: "initial",
-		[Alignments.HorizontallyCentered]: "center",
-		[Alignments.Centered]: "center",
-		[Alignments.TopStart]: "flex-start",
-		[Alignments.TopEnd]: "flex-end",
-		[Alignments.CenteredStart]: "flex-start",
-		[Alignments.CenteredEnd]: "flex-end",
-		[Alignments.BottomStart]: "flex-start",
-		[Alignments.BottomEnd]: "flex-end",
-	}
-
-	const alignItemsMap: Record<Alignments, string> = {
-		[Alignments.Top]: "flex-start",
-		[Alignments.Bottom]: "flex-end",
-		[Alignments.Start]: "initial",
-		[Alignments.End]: "initial",
-		[Alignments.VerticallyCentered]: "center",
-		[Alignments.HorizontallyCentered]: "initial",
-		[Alignments.Centered]: "center",
-		[Alignments.TopStart]: "flex-start",
-		[Alignments.TopEnd]: "flex-start",
-		[Alignments.CenteredStart]: "center",
-		[Alignments.CenteredEnd]: "center",
-		[Alignments.BottomStart]: "flex-end",
-		[Alignments.BottomEnd]: "flex-end",
-	}
-	
 	return css`
 		display: flex;
-        justify-content: ${justifyContentMap[alignment]};
-        align-items: ${alignItemsMap[alignment]};
+		justify-content: ${justifyContentMap[alignment]};
+		align-items: ${alignItemsMap[alignment]};
+	`
+}
+
+export const AlignmentSelf = (alignment: Alignments): SerializedStyles => {
+	return css`
+		display: flex;
+		justify-self: ${justifyContentMap[alignment]};
+		align-self: ${alignItemsMap[alignment]};
 	`
 }
