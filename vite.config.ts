e2f2cd17cd266aa98e588b1836b82dba9d086c9b
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-01-15 23:35:35 +0100

    feat: tweaking config files to make imports easier

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+//@ts-ignore
+import path from "path"
+import { AliasOptions } from "vite"
+
+//@ts-ignore
+const root = path.resolve(__dirname, "src")
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		open: true,
+		open: true
 	},
 	test: {
 		globals: true,
 		environment: "jsdom",
 		setupFiles: "src/setupTests",
-		mockReset: true,
+		mockReset: true
 	},
+	resolve: {
+		alias: {
+			"@": root
+		} as AliasOptions
+	}
 })
