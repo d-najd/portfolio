d3672795faa95485c8929a6daa0d8f5be082104a
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-01-19 19:26:19 +0100

    fix: managed to get window scrolling (and hopefully soon window resizing) to work

--- src/components/WindowContentContainer.tsx
+++ src/components/WindowContentContainer.tsx
@@ -1,13 +1,14 @@
+import styled from "@emotion/styled"
 import {
 	WindowTopBarHeight,
 } from "@/features/window-drawer/components/WindowDrawerTopBar.styles"
-import styled from "@emotion/styled"
+import { Column } from "@/components/Column"
 
 /**
  * Each content for the window much extend this style otherwise horrible things
  * will happen
  */
-export const WindowContentContainer = styled.div`
+export const WindowContentContainer = styled(Column)`
 	position: absolute;
 	width: 100%;
 	height: 100%;
@@ -17,9 +18,10 @@ export const WindowContentContainer = styled.div`
 	// Hours wasted trying to come up with better solution counter: 1.75h
 
 	&:after {
-		height: ${WindowTopBarHeight / 2}px;
+		height: ${WindowTopBarHeight}px;
 		color: rgba(0, 0, 0, 0%);
 		pointer-events: none;
 		user-select: none;
+		content: "Spacer";
 	}
 `
