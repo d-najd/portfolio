575f2b8b1fd6d1adf81f15fdaf7b5bc7563f6039
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-01-19 19:15:15 +0100

    fix: managed to get window scrolling (and hopefully soon window resizing) to work

--- src/components/WindowContentContainer.tsx
+++ src/components/WindowContentContainer.tsx
@@ -1,15 +1,12 @@
 import styled from "@emotion/styled"
 import { WindowTopBarHeight } from "@/features/window-drawer/components/WindowDrawerTopBar.styles"
 
-interface Props {
-	paddingBottom?: number
-}
-
 export const WindowContentContainer = styled.div`
 	position: absolute;
 	width: 100%;
 	height: 100%;
-	overflow: scroll;
+	scrollbar-width: none;
+	resize: both;
 
 	&:after {
 		height: ${WindowTopBarHeight / 2}px;
