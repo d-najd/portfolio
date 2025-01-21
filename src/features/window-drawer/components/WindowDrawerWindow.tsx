24b4fca8e371cedba626fbd9b83d2d14e36e94dd
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-01-21 20:37:52 +0100

    feat!: managed to get resizing to work smoothly and on all sides (needs updating of state though)

--- src/features/window-drawer/components/WindowDrawerWindow.tsx
+++ src/features/window-drawer/components/WindowDrawerWindow.tsx
@@ -1,6 +1,6 @@
 import type { MyWindow } from "@/features/window/windowSlice"
 import useScreenSize from "@/hooks/useScreenSize"
-import React, { useCallback } from "react"
+import React, { useCallback, useRef, useState } from "react"
 import { useAppDispatch, useAppSelector } from "@/app/hooks"
 import {
 	changeActiveWindow,
@@ -13,6 +13,7 @@ import { WindowDrawerTopBar } from "./WindowDrawerTopBar"
 import { GetWindowContentByWindowType } from "@/features/windows/GetWindowContentByWindowType"
 import type { DragState, MousePosition } from "../WindowDrawer"
 import type { Position, Size } from "@/ui/transforms"
+import { Resizable } from "re-resizable"
 
 interface Props {
 	myWindow: MyWindow
@@ -36,6 +37,11 @@ export const WindowDrawerWindow = React.memo(
 		const maximizedWindowId = useAppSelector(selectMaximizedWindowId)
 		const screenSize = useScreenSize()
 
+		const [tempResizeOffset, setTempResizeOffset] = useState<Position>({
+			x: 0,
+			y: 0,
+		})
+
 		const getWindowOffset = useCallback(
 			(curWindow: MyWindow): Position => {
 				if (curWindow.id === maximizedWindowId) {
@@ -47,8 +53,8 @@ export const WindowDrawerWindow = React.memo(
 
 				if (dragState.dragging && curWindow.id === dragState.windowId) {
 					return {
-						x: mousePosition.x - dragState.windowXOffset,
-						y: mousePosition.y - dragState.windowYOffset,
+						x: mousePosition.x + dragState.windowXOffset,
+						y: mousePosition.y + dragState.windowYOffset,
 					}
 				}
 				return {
@@ -86,28 +92,55 @@ export const WindowDrawerWindow = React.memo(
 			[dispatch],
 		)
 
+		let refResize = useRef<Resizable | null>(null)
+
+		// (event: MouseEvent | TouchEvent, direction: Direction, elementRef: HTMLElement, delta: NumberSize)
 		return (
-			<WindowContainer
-				key={myWindow.id}
-				onPointerDown={() => {
-					changeActiveWindowAction(myWindow)
-				}}
+			<Resizable
+				ref={refResize}
 				size={getWindowSize(myWindow)}
-				offset={getWindowOffset(myWindow)}
+				style={resizableContainerStyle(getWindowOffset(myWindow))}
+				onResize={(event, direction, elementRef, delta) => {
+					if (
+						direction === "left" ||
+						direction === "top" ||
+						direction === "topLeft"
+					) {
+						const windowOffset = getWindowOffset(myWindow)
+						elementRef.style.marginLeft =
+							windowOffset.x - delta.width + "px"
+
+						elementRef.style.marginTop =
+							windowOffset.y - delta.height + "px"
+					}
+				}}
 			>
-				<WindowDrawerTopBar
-					myWindow={myWindow}
-					onDragStart={() => {
-						onDragStart(myWindow)
+				<WindowContainer
+					key={myWindow.id}
+					onPointerDown={() => {
+						changeActiveWindowAction(myWindow)
 					}}
-					nonDraggableState={overNonDraggableState}
-					nonDraggableEntered={() => setOverNonDraggableState(true)}
-					nonDraggableExited={() => setOverNonDraggableState(false)}
-				/>
-				<GetWindowContentByWindowType
-					windowType={myWindow.windowType}
-				/>
-			</WindowContainer>
+					size={getWindowSize(myWindow)}
+					offset={getWindowOffset(myWindow)}
+				>
+					<WindowDrawerTopBar
+						myWindow={myWindow}
+						onDragStart={() => {
+							onDragStart(myWindow)
+						}}
+						nonDraggableState={overNonDraggableState}
+						nonDraggableEntered={() =>
+							setOverNonDraggableState(true)
+						}
+						nonDraggableExited={() =>
+							setOverNonDraggableState(false)
+						}
+					/>
+					<GetWindowContentByWindowType
+						windowType={myWindow.windowType}
+					/>
+				</WindowContainer>
+			</Resizable>
 		)
 	},
 )
@@ -117,13 +150,21 @@ interface WindowContainerProps {
 	offset: Position
 }
 
+const resizableContainerStyle = (offset: Position): React.CSSProperties => ({
+	position: "absolute",
+	marginLeft: `${offset.x}px`,
+	marginTop: `${offset.y}px`,
+})
+
 const borderSize = 3
 const WindowContainer = styled.div<WindowContainerProps>`
 	position: absolute;
-	width: ${o => o.size.width}px;
-	height: ${o => o.size.height}px;
-	margin-left: ${o => o.offset.x}px;
-	margin-top: ${o => o.offset.y}px;
+	// width: ${o => o.size.width}px;
+	// height: ${o => o.size.height}px;
+	width: 100%;
+	height: 100%;
+	// margin-left: ${o => o.offset.x}px;
+	// margin-top: ${o => o.offset.y}px;
 	border-top: ${borderSize}px outset ${theme.colors.primaryBorderDepressed};
 	border-left: ${borderSize}px outset ${theme.colors.primaryBorderDepressed};
 	border-right: ${borderSize}px inset ${theme.colors.primaryBorderElevated};
