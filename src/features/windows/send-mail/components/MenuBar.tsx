d299759b5aeccc07ba026e7b1e30afbff3fc6133
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-01-29 16:55:07 +0100

    feat: got menu bar to render correctly

--- src/features/windows/send-mail/components/MenuBar.tsx
+++ src/features/windows/send-mail/components/MenuBar.tsx
@@ -1,99 +1,38 @@
-import { useEffect, useRef, useState } from "react"
+import { useRef } from "react"
 import styled from "@emotion/styled"
 import { Row } from "@/components/Row"
 import { ContentSeparator } from "@/components/ContentSeparator"
+import { useFullyVisibleChildrenCount } from "@/hooks/useFullyVisibleChildrenCount"
+
+interface MenuBarItemData {
+	text: string
+	underlinedIndex: number
+}
 
 export const MenuBar = () => {
 	const containerRef = useRef<HTMLDivElement>(null)
-	const [visibleChildren, setVisibleChildren] = useState(0)
+	const visibleChildren = useFullyVisibleChildrenCount(containerRef)
 
-	const data = [{ name: "ITEM1" }, { name: "ITEM2" }, { name: "ITEM3" }]
-
-	useEffect(() => {
-		if (!containerRef.current) return
-
-		const observer = new ResizeObserver(entries => {
-			entries.forEach(entry => {
-				const parent = entry.target
-				const children = Array.from(parent.children)
-				console.log(entry.target.getBoundingClientRect())
-
-				for (let i = 1; i < children.length; i++) {
-					const child = children[i]
-					if (
-						child.getBoundingClientRect().right >=
-						entry.target.getBoundingClientRect().right
-					) {
-						console.log(child.getBoundingClientRect())
-						console.log(`SETTING ${i}`)
-						setVisibleChildren(i - 1)
-						break
-					} else if (i === children.length - 1) {
-						console.log("HEELLOOO")
-						setVisibleChildren(i)
-					}
-				}
-
-				console.log(visibleChildren)
-			})
-		})
-
-		observer.observe(containerRef.current)
-
-		return () => {
-			observer.disconnect()
-		}
-	}, [containerRef, visibleChildren])
-
-	// setContainerRect(containerRef.current?.getBoundingClientRect() ?? undefined)
-
-	/*
-	useEffect(() => {
-		if (containerRef.current) {
-		}
-	}, [containerRef])
-	
-	 */
-	/*
-			<MenuBarItem disabled={true}>
-				<u>F</u>ile
-			</MenuBarItem>
-			<MenuBarItem disabled={true}>
-				<u>E</u>dit
-			</MenuBarItem>
-			<MenuBarItem disabled={true}>
-				<u>V</u>iew
-			</MenuBarItem>
-			<MenuBarItem disabled={true}>
-				<u>I</u>nsert
-			</MenuBarItem>
-			<MenuBarItem disabled={true}>
-				F<u>o</u>rmat
-			</MenuBarItem>
-			<MenuBarItem disabled={true}>
-				<u>T</u>ools
-			</MenuBarItem>
-			<MenuBarItem disabled={true}>
-				<u>M</u>essage
-			</MenuBarItem>
-			<MenuBarItem disabled={true}>
-				<u>H</u>elp
-			</MenuBarItem>
-	
-				<MenuBarItem text={"Insert"} containerRect={containerRect} />
-			<MenuBarItem text={"Format"} containerRect={containerRect} />
-			<MenuBarItem text={"Tools"} containerRect={containerRect} />
-			<MenuBarItem text={"Message"} containerRect={containerRect} />
-			<MenuBarItem text={"Help"} containerRect={containerRect} />
-	 */
+	const data: MenuBarItemData[] = [
+		{ text: "File", underlinedIndex: 0 },
+		{ text: "Edit", underlinedIndex: 0 },
+		{ text: "View", underlinedIndex: 0 },
+		{ text: "Insert", underlinedIndex: 0 },
+		{ text: "Format", underlinedIndex: 1 },
+		{ text: "Tools", underlinedIndex: 0 },
+		{ text: "Messages", underlinedIndex: 0 },
+		{ text: "Help", underlinedIndex: 0 },
+	]
 
 	return (
 		<Container ref={containerRef}>
 			<Separator />
 			{data.map((e, i) => {
 				return (
-					<Item visible={i < visibleChildren} disabled={true}>
-						{e.name}
+					<Item key={i} visible={i < visibleChildren} disabled={true}>
+						{e.text.slice(0, e.underlinedIndex)}
+						<u>{e.text.charAt(e.underlinedIndex)}</u>
+						{e.text.slice(e.underlinedIndex + 1)}
 					</Item>
 				)
 			})}
@@ -103,8 +42,7 @@ export const MenuBar = () => {
 
 const Container = styled(Row)`
 	border: inset gray 2px;
-	padding: 2px;
-	gap: 25px;
+	padding: 4px;
 	width: 100%;
 	overflow: hidden;
 `
@@ -117,7 +55,7 @@ const Item = styled.button<PropsTest>`
 	padding: 2px 4px;
 	background-color: transparent;
 	border-color: transparent;
-	font-size: 1em;
+	font-size: 1.1em;
 	visibility: ${o => (o.visible === true ? "visible" : "hidden")};
 `
 
---
+++ src/hooks/useFullyVisibleChildrenCount.ts
@@ -0,0 +1,51 @@
+import type React from "react"
+import { useEffect, useRef, useState } from "react"
+
+/**
+ * @param element optional for better compatibility, will always return 0 in that case though
+ * @return number of fully visible items, note this works a bit differently from
+ * what one may expect,
+ * <pre>
+ *     n is the amount of elements
+ *     0: no elements visible
+ *     1...n: elements visibility excluding first and last
+ *     n+1: last element visible
+ * </pre>
+ * @see {useRef}
+ */
+export const useFullyVisibleChildrenCount = (
+	element: React.RefObject<HTMLDivElement>,
+): number => {
+	const [visibleChildren, setVisibleChildren] = useState(0)
+
+	useEffect(() => {
+		if (!element.current) return
+
+		const observer = new ResizeObserver(entries => {
+			const parent = entries[0].target
+			const children = parent.children
+
+			for (let i = 0; i < children.length; i++) {
+				const child = children[i]
+
+				if (
+					child.getBoundingClientRect().right >=
+					parent.getBoundingClientRect().right
+				) {
+					setVisibleChildren(i - 1)
+					break
+				} else if (i === children.length - 1) {
+					setVisibleChildren(i)
+				}
+			}
+		})
+
+		observer.observe(element.current)
+
+		return () => {
+			observer.disconnect()
+		}
+	}, [element])
+
+	return visibleChildren
+}
