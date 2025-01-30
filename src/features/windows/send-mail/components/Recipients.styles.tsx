e0a8813f3209649531b920191422d7db1948eef4
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-01-30 21:20:33 +0100

    feat: added edge icon as well, tweaked styles and refactored them to separate file, Recipients is nearly done

---
+++ src/features/windows/send-mail/components/Recipients.styles.tsx
@@ -0,0 +1,84 @@
+import styled from "@emotion/styled"
+import { Column } from "@/components/Column"
+import theme from "@/theme/theme"
+import { Row } from "@/components/Row"
+import { Alignment, Alignments } from "@/ui/alignment"
+
+const paddingTop = 20
+const paddingBottom = 8
+const rowGap = 8
+const fieldHeight = 18
+
+export const Container = styled.div`
+	display: flex;
+	width: 100%;
+	margin-top: 4px;
+	border: solid black 1px;
+`
+
+export const LeftContainer = styled(Column)`
+	padding: ${paddingTop}px 7px ${paddingBottom}px 7px;
+	gap: ${rowGap}px;
+	text-align: right;
+	background-color: ${theme.colors.windowTopBarInactive};
+	color: white;
+`
+
+export const LeftContainerText = styled.span`
+	height: ${fieldHeight}px;
+	font-weight: 1000;
+`
+
+export const MiddleContainer = styled(Column)`
+	width: 100%;
+	padding: ${paddingTop}px 4px ${paddingBottom}px 4px;
+	gap: ${rowGap}px;
+	overflow: hidden;
+	background-color: white;
+`
+
+export const MiddleSubContainer = styled(Row)`
+	${Alignment(Alignments.VerticallyCentered)};
+`
+
+export const RecipientIcon = styled.img`
+	width: min-content;
+	height: ${fieldHeight}px;
+`
+
+export const MiddleContainerText = styled.u`
+	width: 200px;
+	height: ${fieldHeight}px;
+	margin-left: 4px;
+	font-size: 0.9em;
+`
+
+export const SubjectText = styled.input`
+	height: ${fieldHeight}px;
+	box-sizing: border-box;
+	outline: none;
+	border: none;
+	font-size: 0.9em;
+
+	&:active {
+		border: solid 1px ${theme.colors.windowTopBarInactive};
+	}
+
+	&:focus {
+		border: solid 1px ${theme.colors.windowTopBarInactive};
+	}
+`
+
+export const RightContainer = styled.div`
+	width: 50px;
+	padding: 8px;
+	box-sizing: border-box;
+	background-color: white;
+	${Alignment(Alignments.TopEnd)}
+`
+
+export const EdgeIcon = styled.img`
+	width: min-content;
+	height: 28px;
+	image-rendering: pixelated;
+`
--- src/features/windows/send-mail/components/Recipients.tsx
+++ src/features/windows/send-mail/components/Recipients.tsx
@@ -1,102 +1,36 @@
-import styled from "@emotion/styled"
-import theme from "@/theme/theme"
-import { Column } from "@/components/Column"
 import recipientIcon from "@/resources/icons/send-mail/recipient.png"
-import React from "react"
-import { Row } from "@/components/Row"
-import { Alignment, Alignments } from "@/ui/alignment"
+import edgeIcon from "@/resources/icons/send-mail/edge-1.png"
+import * as S from "./Recipients.styles"
 
 export const Recipients = () => {
 	const mailText = "dimitar.najdovskiw@gmail.com"
 
 	return (
-		<Container>
-			<LeftContainer>
-				<LeftContainerText>To:</LeftContainerText>
-				<LeftContainerText>Cc:</LeftContainerText>
-				<LeftContainerText>Bcc:</LeftContainerText>
-				<LeftContainerText>Subject:</LeftContainerText>
-			</LeftContainer>
-			<RightContainer>
-				<RightSubContainer>
-					<RecipientIcon src={recipientIcon} />
-					<RightContainerText>{mailText}</RightContainerText>
-				</RightSubContainer>
-				<RightSubContainer>
-					<RecipientIcon src={recipientIcon} />
-					<RightContainerText>{mailText}</RightContainerText>
-				</RightSubContainer>
-				<RightSubContainer>
-					<RecipientIcon src={recipientIcon} />
-					<RightContainerText>{mailText}</RightContainerText>
-				</RightSubContainer>
-				<SubjectText></SubjectText>
-			</RightContainer>
-		</Container>
+		<S.Container>
+			<S.LeftContainer>
+				<S.LeftContainerText>To:</S.LeftContainerText>
+				<S.LeftContainerText>Cc:</S.LeftContainerText>
+				<S.LeftContainerText>Bcc:</S.LeftContainerText>
+				<S.LeftContainerText>Subject:</S.LeftContainerText>
+			</S.LeftContainer>
+			<S.MiddleContainer>
+				<S.MiddleSubContainer>
+					<S.RecipientIcon src={recipientIcon} />
+					<S.MiddleContainerText>{mailText}</S.MiddleContainerText>
+				</S.MiddleSubContainer>
+				<S.MiddleSubContainer>
+					<S.RecipientIcon src={recipientIcon} />
+					<S.MiddleContainerText>{mailText}</S.MiddleContainerText>
+				</S.MiddleSubContainer>
+				<S.MiddleSubContainer>
+					<S.RecipientIcon src={recipientIcon} />
+					<S.MiddleContainerText>{mailText}</S.MiddleContainerText>
+				</S.MiddleSubContainer>
+				<S.SubjectText></S.SubjectText>
+			</S.MiddleContainer>
+			<S.RightContainer>
+				<S.EdgeIcon src={edgeIcon}></S.EdgeIcon>
+			</S.RightContainer>
+		</S.Container>
 	)
 }
-
-const paddingTop = 16
-const paddingBottom = 8
-const rowGap = 8
-const fieldHeight = 18
-
-const Container = styled.div`
-	margin-top: 4px;
-	display: flex;
-	width: 100%;
-	border: solid black 1px;
-`
-
-const LeftContainer = styled(Column)`
-	gap: ${rowGap}px;
-	text-align: right;
-	width: fit-content;
-	padding: ${paddingTop}px 7px ${paddingBottom}px 7px;
-	background-color: ${theme.colors.windowTopBarInactive};
-	color: white;
-`
-
-const LeftContainerText = styled.span`
-	font-weight: 1000;
-	height: ${fieldHeight}px;
-`
-
-const RightContainer = styled(Column)`
-	gap: ${rowGap}px;
-	padding: ${paddingTop}px 4px ${paddingBottom}px 4px;
-	width: 100%;
-	background-color: white;
-`
-
-const RightSubContainer = styled(Row)`
-	${Alignment(Alignments.VerticallyCentered)};
-`
-
-const RecipientIcon = styled.img`
-	height: ${fieldHeight}px;
-	width: min-content;
-`
-
-const RightContainerText = styled.u`
-	width: 200px;
-	height: ${fieldHeight}px;
-	font-size: 0.9em;
-	margin-left: 4px;
-`
-
-const SubjectText = styled.input`
-	box-sizing: border-box;
-	height: ${fieldHeight}px;
-	outline: none;
-	border: none;
-	font-size: 0.9em;
-
-	&:active {
-		border: solid 1px ${theme.colors.windowTopBarInactive};
-	}
-
-	&:focus {
-		border: solid 1px ${theme.colors.windowTopBarInactive};
-	}
-`
