a55e3f2f216bf9f2e4417d424ad4f44e1948c464
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-01-30 20:51:15 +0100

    feat!: got most of recipients menu working

--- src/features/windows/send-mail/components/Recipients.tsx
+++ src/features/windows/send-mail/components/Recipients.tsx
@@ -1,8 +1,14 @@
 import styled from "@emotion/styled"
 import theme from "@/theme/theme"
 import { Column } from "@/components/Column"
+import recipientIcon from "@/resources/icons/send-mail/recipient.png"
+import React from "react"
+import { Row } from "@/components/Row"
+import { Alignment, Alignments } from "@/ui/alignment"
 
 export const Recipients = () => {
+	const mailText = "dimitar.najdovskiw@gmail.com"
+
 	return (
 		<Container>
 			<LeftContainer>
@@ -11,32 +17,86 @@ export const Recipients = () => {
 				<LeftContainerText>Bcc:</LeftContainerText>
 				<LeftContainerText>Subject:</LeftContainerText>
 			</LeftContainer>
-			<RightContainer></RightContainer>
+			<RightContainer>
+				<RightSubContainer>
+					<RecipientIcon src={recipientIcon} />
+					<RightContainerText>{mailText}</RightContainerText>
+				</RightSubContainer>
+				<RightSubContainer>
+					<RecipientIcon src={recipientIcon} />
+					<RightContainerText>{mailText}</RightContainerText>
+				</RightSubContainer>
+				<RightSubContainer>
+					<RecipientIcon src={recipientIcon} />
+					<RightContainerText>{mailText}</RightContainerText>
+				</RightSubContainer>
+				<SubjectText></SubjectText>
+			</RightContainer>
 		</Container>
 	)
 }
 
-const paddingVertical = 12
+const paddingTop = 16
+const paddingBottom = 8
+const rowGap = 8
+const fieldHeight = 18
 
 const Container = styled.div`
+	margin-top: 4px;
 	display: flex;
 	width: 100%;
 	border: solid black 1px;
 `
 
 const LeftContainer = styled(Column)`
-	gap: 8px;
+	gap: ${rowGap}px;
 	text-align: right;
 	width: fit-content;
-	padding: ${paddingVertical}px 6px;
+	padding: ${paddingTop}px 7px ${paddingBottom}px 7px;
 	background-color: ${theme.colors.windowTopBarInactive};
 	color: white;
-	font-weight: bolder;
 `
 
-const LeftContainerText = styled.span``
+const LeftContainerText = styled.span`
+	font-weight: 1000;
+	height: ${fieldHeight}px;
+`
 
-const RightContainer = styled.div`
-	padding: ${paddingVertical}px 0;
+const RightContainer = styled(Column)`
+	gap: ${rowGap}px;
+	padding: ${paddingTop}px 4px ${paddingBottom}px 4px;
 	width: 100%;
+	background-color: white;
+`
+
+const RightSubContainer = styled(Row)`
+	${Alignment(Alignments.VerticallyCentered)};
+`
+
+const RecipientIcon = styled.img`
+	height: ${fieldHeight}px;
+	width: min-content;
+`
+
+const RightContainerText = styled.u`
+	width: 200px;
+	height: ${fieldHeight}px;
+	font-size: 0.9em;
+	margin-left: 4px;
+`
+
+const SubjectText = styled.input`
+	box-sizing: border-box;
+	height: ${fieldHeight}px;
+	outline: none;
+	border: none;
+	font-size: 0.9em;
+
+	&:active {
+		border: solid 1px ${theme.colors.windowTopBarInactive};
+	}
+
+	&:focus {
+		border: solid 1px ${theme.colors.windowTopBarInactive};
+	}
 `
