e41cb110dcb7c01e96186c2ff638d48e2cfe1c7a
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-10-13 17:42:26 +0200

    feat!: getting basic shader to render

--- src/vertex.glsl
+++ src/vertex.glsl
@@ -4,5 +4,6 @@
 
  void main() {
      gl_Position = vec4(a_position, 0.0, 1.0);
-     v_color = a_color;
+     // v_color = a_color;
+     v_color = vec3(a_position.x + 0.5, a_position.y + 0.5, a_position.x + 0.5);
  }
